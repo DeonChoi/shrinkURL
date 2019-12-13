c2c19201c2878aba0fc9985d76f6b900742cc203
Deon Choi <deon.choi@gmail.com>
2019-12-12 18:44:30 -0800

    heroku initialization

---
+++ backend/build/precache-manifest.ddf551a748dce0f1a3d5ab80f30c1717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05233d7592b1c7a2efde307ce2711916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f98cfe6d8ad9197f335",
+    "url": "/static/css/main.3881dcca.chunk.css"
+  },
+  {
+    "revision": "1955fb1875ad56cdef0b",
+    "url": "/static/js/2.61849bae.chunk.js"
+  },
+  {
+    "revision": "3f98cfe6d8ad9197f335",
+    "url": "/static/js/main.67e30f86.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  },
+  {
+    "revision": "7480586cbf73bea34a66f6b983cae023",
+    "url": "/static/media/homeImage.7480586c.png"
+  },
+  {
+    "revision": "b5adfc36d6e87c06355dbdd342bb4d2e",
+    "url": "/static/media/navbarBrand.b5adfc36.png"
+  }
+]);
--- backend/server.js
+++ backend/server.js
@@ -25,13 +25,6 @@ const redirectRouter = require('./routes/index');
 const authRoute = require('./routes/auth');
 
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, 'build')));
-    
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//     });
-// }
 
 
 
@@ -44,6 +37,14 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, 'build')));
+    
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+//     });
+// }
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
