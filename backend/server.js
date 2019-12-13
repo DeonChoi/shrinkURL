55fe275e4e9a68750c7df50919a3d45df259a937
Deon Choi <deon.choi@gmail.com>
2019-12-12 19:29:09 -0800

    reconfigured server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
-require('dotenv').config();
+
 
 const app = express();
 const port = process.env.PORT || 3000;
